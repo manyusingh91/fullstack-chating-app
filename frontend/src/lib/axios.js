48167309ba7008391912b99e2b971ad0e36ec43b
Abhimany singh <singhabhimanyu9768@gmail.com>
2025-02-13 16:20:58 +0530

    Project completed

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:9090/api" : "/api",
+  withCredentials: true,
+});
